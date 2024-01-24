5da02d09fc574aef6fcc5c12a6e6514a18dc1c38
richie1988 <richiessikaonga@gmail.com>
2024-01-24 17:34:15 +0200

    Backend rendering

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,12 @@ import "@hotwired/turbo-rails";
 import "./controllers";
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navbar from './navbar/navigation';
 
 import App from './components/App';
-import Navbar from "./navbar/navigation";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -15,10 +15,9 @@ root.render(
   <Provider store={store}>
     <Router>
       <h1>React Client Side Rendering</h1>
-      <NavbarComponets />
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/page" element={<App />} />
+        <Route path="/page" element={<Navbar />} />
       </Routes>
     </Router>
   </Provider>
